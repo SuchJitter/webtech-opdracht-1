3b1d53aa700e669389427c667401b500a07e8ef5
tjibster <45420236+tjibster@users.noreply.github.com>
2022-03-14 11:52:45 +0100

    food subclasses
    subclasses

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // var menuElement = document.getElementById("menubox");
 // var paragraph = document.createElement("p");
 // var text = document.createTextNode();
@@ -14,10 +15,22 @@ class menu {
 }
 class food {
     constructor(voedsel){
+=======
+var menuElement = document.getElementById("menubox");
+var paragraph = document.createElement("p");
+var text = document.createTextNode("dynamisch gegenereert");
+paragraph.appendChild(text);
+menuElement.appendChild(paragraph);
+
+
+class food {
+    constructor(voedsel, naam, prijs, vega){
+>>>>>>> Stashed changes
         var menuElement = document.getElementById("menubox");
         var paragraph = document.createElement("p");
         var text = document.createTextNode(voedsel);
         paragraph.appendChild(text);
+<<<<<<< Updated upstream
         menuElement.appendChild(paragraph);
     }
 }
@@ -33,3 +46,53 @@ class menuSection {
 var menus = new menu();
 var ontbijt = menus.createNewMenuSection();
 ontbijt.createNewFood("eieren");
+=======
+        menuElement.appendChild(paragrap);
+        this.naam = naam;
+        this.prijs = prijs;
+        this.vega = vega;
+    }
+}
+
+class voorgerecht extends food {
+    constructor(naam, prijs, vega){
+        super(naam);
+        super(prijs);
+        super(vega);
+    }
+}
+
+class hoofgerecht extends food {
+    constructor(naam, prijs, vega){
+        super(naam);
+        super(prijs);
+        super(vega);
+    }
+}
+
+class nagerecht extends food {
+    constructor(naam, prijs, vega){
+        super(naam);
+        super(prijs);
+        super(vega);
+    }
+}
+
+class drinken extends food {
+    constructor(naam, prijs, vega, percentage){
+        super(naam);
+        super(prijs);
+        super(vega);
+        this.percentage = percentage;
+    }
+}
+
+class borrelHap extends food {
+    constructor(naam, prijs, vega, hoeveelheid){
+        super(naam);
+        super(prijs);
+        super(vega);
+        this.hoeveelheid = hoeveelheid;
+    }
+}
+>>>>>>> Stashed changes

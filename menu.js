7c7e7eec68ddbd54749f5d21e4dc423980db9264
SuchJitter <89609527+SuchJitter@users.noreply.github.com>
2022-03-14 12:39:45 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,46 +1,64 @@
-// var menuElement = document.getElementById("menubox");
+var menuElement = document.getElementById("menubox");
 // var paragraph = document.createElement("p");
 // var text = document.createTextNode();
 // paragraph.appendChild(text);
 // menuElement.appendChild(paragraph);
 
+function createTableHeader(description) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(description);
+    th.appendChild(text);
+    return th;
+}
+
 class menu {
     constructor(){
-        var menuSectionArray = [];
     }
     createNewMenuSection() {
         return new menuSection();
     }
 }
 
-class food {
-    constructor(voedsel, naam, prijs, vega){
-        var menuElement = document.getElementById("menubox");
-        var paragraph = document.createElement("p");
-        var text = document.createTextNode(voedsel);
-        paragraph.appendChild(text);
-        menuElement.appendChild(paragraph);
-        this.naam = naam;
-        this.prijs = prijs;
-        this.vega = vega;
-    }
-}
-
 class menuSection {
     constructor(){
+        var tabel = document.createElement("table");
+        let tr = document.createElement("tr");
+        let gerecht = createTableHeader("gerecht");
+        let vegetarisch = createTableHeader("Vega");
+        let spicy = createTableHeader("Spicy");
+        let prijs = createTableHeader("Prijs");
+        tabel.appendChild(tr);
+        tr.appendChild(gerecht);
+        tr.appendChild(vegetarisch);
+        tr.appendChild(spicy);
+        tr.appendChild(prijs);
+        menuElement.appendChild(tabel);
     }
     createNewFood(voedsel) {
         let gerecht = new food(voedsel);
+        tabel.appendChild(gerecht);
     }
 }
 
-var menus = new menu();
-var ontbijt = menus.createNewMenuSection();
-ontbijt.createNewFood("eieren");
-        menuElement.appendChild(paragrap);
+class food {
+    constructor(naam, prijs, vega){
         this.naam = naam;
         this.prijs = prijs;
         this.vega = vega;
+        var master = document.createElement("tr");
+        var naam = document.createElement("td");
+        var prijs = document.createElement("td");
+        var vega = document.createElement("td");
+        var naamText = document.createTextNode(naam);
+        var prijsText = document.createTextNode(prijs);
+        var vegaText = document.createTextNode(vega);
+        naam.appendChild(naamText);
+        prijs.appendChild(prijsText);
+        vega.appendChild(vegaText);
+        master.appendChild(naam);
+        master.appendChild(vega);
+        master.appendChild(prijs);
+        return master;
     }
 }
 
@@ -85,3 +103,7 @@ class borrelHap extends food {
         this.hoeveelheid = hoeveelheid;
     }
 }
+
+let menus = new menu();
+let menusectie = menus.createNewMenuSection();
+menusectie.createNewFood("ei", "yes", 123)

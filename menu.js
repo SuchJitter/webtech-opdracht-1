4324cebd08c9567e8b16bd2a60d80a08c8f5aa2c
SuchJitter <89609527+SuchJitter@users.noreply.github.com>
2022-03-14 11:28:53 +0100

    dingen

---
+++ menu.js
@@ -0,0 +1,5 @@
+var menuElement = document.getElementById("menubox");
+var paragraph = document.createElement("p");
+var text = document.createTextNode("dynamisch gegenereert");
+paragraph.appendChild(text);
+menuElement.appendChild(paragraph);

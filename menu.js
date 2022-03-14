7beaa2384092bf7162c5f2a5d523bcef806c1831
SuchJitter <89609527+SuchJitter@users.noreply.github.com>
2022-03-14 11:33:27 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,3 +3,25 @@ var paragraph = document.createElement("p");
 var text = document.createTextNode("dynamisch gegenereert");
 paragraph.appendChild(text);
 menuElement.appendChild(paragraph);
+
+class menu {
+    constructor(){
+        var menuSectionArray = [];
+    }
+}
+
+class menuSection {
+    constructor(){
+        var FoodArray = [];
+    }
+}
+
+class food {
+    constructor(voedsel){
+        var menuElement = document.getElementById("menubox");
+        var paragraph = document.createElement("p");
+        var text = document.createTextNode(voedsel);
+        paragraph.appendChild(text);
+        menuElement.appendChild(paragraph);
+    }
+}
